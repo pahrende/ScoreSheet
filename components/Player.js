454a1dfdd907a8b1bcd185778134722b2b4d14c8
Jessica Clarke <eriknclarke@gmail.com>
2020-01-15 21:11:04 -0700

    added basic player layout

--- components/Player.js
+++ components/Player.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import {
+    View,
+    Text,
+    StyleSheet
+} from 'react-native';
 
 export default class Player extends React.Component{
 
+    state = {
+        playerInfo: {
+            name: "Player",
+            color: "blue",
+            score: 0,
+            scoreHistory: []
+        }
+    }
+
     render(){
-        
+        const playerInfo = this.state.playerInfo;
+
+        return(
+            <View style={styles.tableContainer}>
+                <View style={styles.tableRow}>
+                    <View style={styles.tableCell}><Text>{playerInfo.name}</Text></View>
+                    <Button><Text>+</Text></Button>
+                    <View style={styles.tableCell}><Text>{playerInfo.color}</Text></View>
+                    <Button><Text>-</Text></Button>
+                    <View style={styles.tableCell}><Text>{playerInfo.score}</Text></View>
+                </View>
+            </View>
+        );
     }
 }
+
+const styles = StyleSheet.create({
+    tableContainer:{ flex: 1, alignItems:'center', justifyContent: 'center',paddingTop: 30, borderWidth: 0.5, borderColor: "#c8e1ff", minWidth: 280 },
+    tableRow:{ flex: 1, flexDirection: 'row', alignSelf: 'stretch', borderWidth: 0.5, borderColor: "#c8e1ff" },
+    tableCell: { flex: 1, alignSelf: 'stretch', borderWidth: 0.5, borderColor: "#c8e1ff"}
+  });
